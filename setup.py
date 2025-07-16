84e9aa5bb82fc0065053e1bfe8a642edc529ca0d
shvr <40132076-shvrs@users.noreply.replit.com>
2025-07-16 21:33:30 +0000

    Update code to match latest replit changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Discordia",
+    version="0.1.0",
+    author="shvrs",
+    description="A simple Discord API wrapper.",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
